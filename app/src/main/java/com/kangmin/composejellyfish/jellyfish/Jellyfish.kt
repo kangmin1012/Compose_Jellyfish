cf32d4dd02016d9c4550db6d122825c2831c2a1a
kangmin1012 <kangmin1012@naver.com>
2022-12-29 14:37:34 +0900

    해파리 눈 깜빡임 애니메이션

--- app/src/main/java/com/kangmin/composejellyfish/jellyfish/Jellyfish.kt
+++ app/src/main/java/com/kangmin/composejellyfish/jellyfish/Jellyfish.kt
@@ -1,15 +1,21 @@
 package com.kangmin.composejellyfish.jellyfish
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.Group
 import androidx.compose.ui.graphics.vector.Path
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kangmin.composejellyfish.background.blueRadialGradient
@@ -17,6 +23,8 @@ import com.kangmin.composejellyfish.ui.theme.ComposeJellyfishTheme
 import com.kangmin.composejellyfish.ui.theme.Gray
 import com.kangmin.composejellyfish.ui.theme.Gray300
 import com.kangmin.composejellyfish.ui.theme.White300
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 private val solidWhiteColor = SolidColor(Color.White)
 private val solidWhite300Color = SolidColor(White300)
@@ -25,17 +33,52 @@ private val solidGray300Color = SolidColor(Gray300)
 
 @Composable
 fun Jellyfish() {
+    val rememberCoroutineScope = rememberCoroutineScope()
+    val blinkAlphaAnimation = remember {
+        Animatable(1f)
+    }
+    val blinkScaleAnimation = remember {
+        Animatable(1f)
+    }
+
+    suspend fun startBlinkAnimation() {
+        val tweenSpec = tween<Float>(durationMillis = 150, easing = LinearEasing)
+        coroutineScope {
+            launch {
+                blinkAlphaAnimation.animateTo(targetValue = 0f, animationSpec = tweenSpec)
+                blinkAlphaAnimation.animateTo(targetValue = 1f, animationSpec = tweenSpec)
+            }
+            launch {
+                blinkScaleAnimation.animateTo(targetValue = 0.3f, animationSpec = tweenSpec)
+                blinkScaleAnimation.animateTo(targetValue = 1f, animationSpec = tweenSpec)
+            }
+        }
+    }
+
+    /** 해파리 vector 이미지 */
     val vectorPainter = rememberVectorPainter(
         defaultWidth = 530.46f.dp,
         defaultHeight = 563.1f.dp,
         viewportHeight = 530.46f,
         viewportWidth = 563.1f,
         autoMirror = true
-    ) { _, _ ->
+    ) { viewportWiddth, viewportHeight ->
+
+        val duration = 3000 // 애니메이션 시간
+        val transition = rememberInfiniteTransition() // 무한 애니메이션 만들기
+        val translationY by transition.animateFloat(
+            initialValue = 0f,
+            targetValue = -30f,
+            animationSpec = infiniteRepeatable(
+                tween(durationMillis = duration, easing = EaseInOut),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
 
         // 해파리의 몸체 VectorDrawable
         Group(
-            name = "jellyfish"
+            name = "jellyfish",
+            translationY = translationY
         ) {
             // 해파리 촉수 Vector Group
             Group(name = "tentacles") {
@@ -116,14 +159,22 @@ fun Jellyfish() {
             }
             // 해파리 얼굴 Vector Group
             Group(name = "face") {
-                Path(
-                    pathData = JellyFishPaths.leftEye,
-                    fill = solidGrayColor
-                )
-                Path(
-                    pathData = JellyFishPaths.rightEye,
-                    fill = solidGrayColor
-                )
+                Group(
+                    name = "eye",
+                    scaleY = blinkScaleAnimation.value,
+                    pivotY = 233f
+                ) {
+                    Path(
+                        pathData = JellyFishPaths.leftEye,
+                        fill = solidGrayColor,
+                        fillAlpha = blinkAlphaAnimation.value
+                    )
+                    Path(
+                        pathData = JellyFishPaths.rightEye,
+                        fill = solidGrayColor,
+                        fillAlpha = blinkAlphaAnimation.value
+                    )
+                }
                 Path(
                     pathData = JellyFishPaths.mouth,
                     fill = solidGray300Color
@@ -172,6 +223,13 @@ fun Jellyfish() {
         contentDescription = "Jellyfish",
         modifier = Modifier
             .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures {
+                    rememberCoroutineScope.launch {
+                        startBlinkAnimation()
+                    }
+                }
+            }
             .background(blueRadialGradient)
     )
 }
